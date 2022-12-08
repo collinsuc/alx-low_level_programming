b374449acb13ed2231511fd216fc0fac9d47b8e9
Collins <collins.ahuchaogu@gmail.com>
2022-12-08 15:56:50 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,13 @@
-#include <stdio.h>                                                                                                                      
+#include <stdio.h>
+
+                                                                                                                      
 
 /**                                                                                                                                     
+ * main - Entry                                                                                                    
  *
- *  * main - Entry                                                                                                                         
- *
- *   * Return: Always 0                                                                                                                     
- *
- *    */                                                                                                                                     
-
-int main(void)                                                                                                                          
-
+ * Return: Always 0
+ */                                                                                                              
+int main(void) 
 { puts("\"Programming is like building a multi lingual puzzle");                                                            
-
-	  return (0);
-
-}
+  return (0);
+} 
