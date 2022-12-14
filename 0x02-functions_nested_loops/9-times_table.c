612e9310f923b14227e89bbc1ca66c9a72696a49
Collins <collins.ahuchaogu@gmail.com>
2022-12-13 16:50:41 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,6 +24,7 @@ void times_table(void)
 				{
 					_putchar(' ');
 					_putchar((c % 10) + '0');
+				}
 				if (b < 9)
 				{
 					_putchar(',');
@@ -31,7 +32,7 @@ void times_table(void)
 				}
 			}
 			else
-			}
+			{
 				_putchar ((c / 10) + '0');
 				_putchar ((c % 10) + '0');
 				if (b < 9)
